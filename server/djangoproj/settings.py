1205f364abfbd841054c4a14bb7f28a18c7a0ff3
Holly Henske <hcover333@gmail.com>
2025-09-11 16:14:10 -0400

    get cars working in admon site

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -49,7 +49,8 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS=['localhost','https://hcover333-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS=['https://hcover333-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
+CSRF_TRUSTED_ORIGINS=['https://hcover333-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+     "https://hcover333-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai"]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
