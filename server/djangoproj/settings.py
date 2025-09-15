a4b5c6e22b5664c8d83c89217670bed73a73c8b9
Holly Henske <hcover333@gmail.com>
2025-09-15 08:38:37 -0400

    post review code finished. Errors persist

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -48,7 +48,7 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS=['localhost','https://hcover333-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
+ALLOWED_HOSTS=["127.0.0.1", 'localhost','https://hcover333-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
 CSRF_TRUSTED_ORIGINS=['https://hcover333-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
      "https://hcover333-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai"]
 
