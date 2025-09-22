750f11c650fc6e0c13795fce34c4675c1ea7d850
Holly Henske <hcover333@gmail.com>
2025-09-22 07:35:42 -0400

    linter: down to a couple of errors (fixed)

--- server/djangoapp/migrations/0001_initial.py
+++ server/djangoapp/migrations/0001_initial.py
@@ -59,7 +59,8 @@ class Migration(migrations.Migration):
                 ,
             ('car_make', models.ForeignKey(
                 on_delete=django.db.models.deletion.CASCADE,
-                to='djangoapp.carmake')
+                to='djangoapp.carmake'
+                    )
                 ),
             ],
         ),
--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -134,22 +134,22 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-            'django.contrib.auth.password_validation.' \
+            'django.contrib.auth.password_validation.'
             'UserAttributeSimilarityValidator'
     },
     {
         'NAME':
-            'django.contrib.auth.password_validation.' \
+            'django.contrib.auth.password_validation.'
             'MinimumLengthValidator'
     },
     {
         'NAME':
-            'django.contrib.auth.password_validation.' \
+            'django.contrib.auth.password_validation.'
             'CommonPasswordValidator'
     },
     {
         'NAME':
-            'django.contrib.auth.password_validation.' \
+            'django.contrib.auth.password_validation.'
             'NumericPasswordValidator'
     },
 ]
